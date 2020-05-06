d1da737aeed36aadc3a5325039b95119418c1ca4
Holisha <fan89511@gmail.com>
2020-05-06 23:34:08 +0800

    add stroke count in utils

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 import pandas as pd
 import numpy as np
-import torch
-from  torchvision import transforms
+
 
 test = pd.read_csv('char00436_stroke.csv', header=None)
-arr = test.iloc[:, :-1].to_numpy()
+tmp = test.groupby(6)[0].count().max()
 
-# print(torch.from_numpy(arr))
-print(transforms.ToTensor()(arr))
+print(tmp)
+print(type(tmp))
+
+
+
+""" import pandas as pd
+import os
+from glob import glob
+
+
+def stroke_statistics(path='6d/', rulke):
+    
+
+
+if __name__ == '__main__':
+    stroke_statistics() """
--- utils.py
+++ utils.py
@@ -1,2 +1,27 @@
+import pandas as pd
+import numpy as np
+import os
+
+def stroke_statistics(path='6d/'):
+    max_cnt = np.int64(0)
+    mean_cnt = np.int64(0)
+
+    for dir_name in os.listdir(path):
+        df = pd.read_csv(
+            os.path.join(path, dir_name, dir_name + '.csv'),
+            header=None
+        )
+
+        tmp = df.groupby(6)[0].count()
+
+        if tmp.max() > max_cnt:
+            max_cnt = tmp.max()
+        
+        if tmp.mean() > mean_cnt:
+            mean_cnt = tmp.mean()
+    
+    print(f'max stroke number:{max_cnt}\nmean stroke number:{mean_cnt}')
 
 
+if __name__ == '__main__':
+    stroke_statistics()
