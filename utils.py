3432bff551c6650d3b358ef1be158f151f6da05b
Holisha <fan89511@gmail.com>
2020-05-07 01:02:15 +0800

    add comment & return value

--- utils.py
+++ utils.py
@@ -2,9 +2,17 @@ import pandas as pd
 import numpy as np
 import os
 
-def stroke_statistics(path='6d/'):
+def stroke_statistics(path='6d/', mode='max'):
+    r""" 
+    parameter:
+    path: path of the 6d axis csv file
+    mode: output statstic. i.e. mode:max means return the maximum stroke number
+    count the mean, maximum, minimum of the stroke statistics
+    output: use parameter 
+    """
     max_cnt = np.int64(0)
     mean_cnt = np.int64(0)
+    min_cnt = np.int64(100)
 
     for dir_name in os.listdir(path):
         df = pd.read_csv(
@@ -19,9 +27,19 @@ def stroke_statistics(path='6d/'):
         
         if tmp.mean() > mean_cnt:
             mean_cnt = tmp.mean()
+
+        if tmp.min() < min_cnt:
+            min_cnt = tmp.min()
     
-    print(f'max stroke number:{max_cnt}\nmean stroke number:{mean_cnt}')
+    print(f'max stroke number:{max_cnt}\nmean stroke number:{mean_cnt}\nmin stroke number:{min_cnt}')
+
+    return {
+        'max' : max_cnt,
+        'mean': mean_cnt,
+        'min' : min_cnt
+    }.get(mode, 'error')
 
 
 if __name__ == '__main__':
-    stroke_statistics()
+    string = stroke_statistics()
+    print(f'test: {string}')
