58b0877487a6fda05bc5bef354799dea2358f434
Holisha <fan89511@gmail.com>
2020-05-10 09:18:08 +0800

    add pytorch lightning & utils

--- main.py
+++ main.py
@@ -1,66 +1,17 @@
-import argparse
 import os
+import sys
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import csv
 import numpy as np
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 
-from models import FSRCNN
+from models import FSRCNN, LightFSRCNN
 from dataset import AxisDataSet
-
-def train(model, device, train_loader, optimizer, criterion, args):
-    best_err = None
-
-    for epoch in range(args.num_epochs):
-        model.train()
-        err = 0.0
-
-        for data in tqdm(train_loader, desc=f'epoch: {epoch+1}/{args.num_epochs}'):
-            inputs, target = data
-            inputs, target = inputs.to(device), target.to(device)
-            pred = model(inputs)
-            loss = criterion(pred, target)
-            err += loss.sum().item()
-
-            if ((epoch + 1) % (args.num_epochs / 10) == 0):
-                # output 6axis of inputs, pred and target to csv file
-                out2csv(inputs, str(epoch + 1) + '_input', args.stroke_length)
-                out2csv(pred, str(epoch + 1) + '_output', args.stroke_length)
-                out2csv(target, str(epoch + 1) + '_target', args.stroke_length)
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        err /= len(train_loader)
-        print(f'loss: {err:.4f}')
-        # update every epoch
-        if best_err is None or err < best_err:
-            best_err = err
-            torch.save(model.state_dict(), f'fsrcnn_{args.scale}x.pt')
-
-
-def test(model, device, test_loader, criterion, args):
-
-    model.load_state_dict(torch.load(f'fsrcnn_{args.scale}x.pt'))
-    model.eval()
-    err = 0.0
-
-    with torch.no_grad():
-
-        for data in tqdm(test_loader, desc=f'scale: {args.scale}'):
-            inputs, target = data
-            inputs, target = inputs.to(device), target.to(device)
-
-            pred = model(inputs)
-            loss = criterion(pred, target)
-            err += loss.sum().item()
-            # out2csv(pred, "test_final")
-    
-    print(f'test error:{err:.4f}')
+from train import train
+from test import test
+from utils import argument_setting
 
 def out2csv(input, file_string, stroke_length):
     '''
@@ -86,16 +37,6 @@ def out2csv(input, file_string, stroke_length):
             writer.writerow(row)
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, default='./train')
-    parser.add_argument('--batch-size', type=int, default=64)
-    parser.add_argument('--num-workers', type=int, default=4)
-    parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--scale', type=int, default=1)
-    parser.add_argument('--num-epochs', type=int, default=10)
-    parser.add_argument('--stroke-length', type=int, default=59)
-
-    args = parser.parse_args()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     # print('device='+str(device))
     train_set = AxisDataSet(args.path)
@@ -123,5 +64,26 @@ def main():
     test(model, device, test_loader, criterion, args)
 
 
+def light():
+    from pytorch_lightning import Trainer
+    from pytorch_lightning.loggers import TensorBoardLogger
+
+    logger = TensorBoardLogger('./logs', name='FSRCNN_light')
+    model = LightFSRCNN(args)
+    trainer = Trainer(
+        logger=logger,
+        max_epochs=args.epochs,
+        gpus=1
+    )
+
+    trainer.fit(model)
+    trainer.test()
+
+
 if __name__ == '__main__':
-    main()
+    args = argument_setting()
+
+    if sys.platform.startswith('win'):
+        main()
+    else:
+        light()
--- utils.py
+++ utils.py
@@ -1,2 +1,16 @@
+from argparse import ArgumentParser
+
+
+def argument_setting():
+    parser = ArgumentParser()
+    parser.add_argument('--train-path', type=str, default='./train')
+    parser.add_argument('--test-path', type=str, default='./test')
+    parser.add_argument('--batch-size', type=int, default=64)
+    parser.add_argument('--num-workers', type=int, default=4)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--scale', type=int, default=1)
+    parser.add_argument('--epochs', type=int, default=5)
+
+    return parser.parse_args()
 
 
