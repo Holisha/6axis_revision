fcc6574f9b230d7dd7f1bb3da982652efa9b0285
lyjeff <38328932+lyjeff@users.noreply.github.com>
2020-07-04 19:22:14 +0800

    Add preprocess and update csv2txt
    1. Add  preprocess
    2. Update csv2txt.py :
            改成遞迴方式，把資料夾底下全部csv檔轉成txt檔。

--- csv2txt.py
+++ csv2txt.py
@@ -1,6 +1,5 @@
 import csv
 import os
-from glob import glob
 
 def main(path='./output'):
 	"""Convert all CSV files to TXT files.
@@ -8,19 +7,29 @@ def main(path='./output'):
 	Keyword Arguments:
 		path {str} -- Path to output directory (default: {'./output'})
 	"""
-	for csv_name in sorted(glob(os.path.join(path, '*.csv'))):
-		# read csv file content
-		with open(csv_name, newline='') as csv_file:
-			rows = csv.reader(csv_file)
-			txt_name = f'{csv_name[:-4]}.txt'
-			# store in txt file
-			with open(txt_name, "w") as txt_file:
-				for row in rows:
-					txt_file.write("movl 0 ")
-					for j in range(len(row) - 1):
-						txt_file.write(f'{float(row[j]):0.4f} ')
-					txt_file.write("100.0000 ")
-					txt_file.write(f'{row[6]}\n')
+	for root, dirs, files in os.walk(path):
+		for f in files:
+			# if not csv file
+			if (f[-4:] != '.csv'):
+				continue;
+			
+			csv_name = os.path.join(root, f);
+			print(f'read\t{csv_name}')
+			
+			# read csv file content
+			with open(csv_name, newline='') as csv_file:
+				rows = csv.reader(csv_file)
+				txt_name = f'{csv_name[:-4]}.txt'
+				
+				# store in txt file
+				with open(txt_name, "w") as txt_file:
+					for row in rows:
+						txt_file.write("movl 0 ")
+						for j in range(len(row) - 1):
+							txt_file.write(f'{float(row[j]):0.4f} ')
+						txt_file.write("100.0000 ")
+						txt_file.write(f'{row[6]}\n')
+					print(f'write\t{txt_name}')
 
 if __name__ == '__main__':
 	main()
