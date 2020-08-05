f0ec485a7f7c0119987dc44ba9c09b16c6bb117a
lyjeff <38328932+lyjeff@users.noreply.github.com>
2020-08-05 23:58:01 +0800

    Optimize some place in postprocessing 1. Optimize some place in postprocessing 2. Rename preprocess folder name from 'preprocess' to 'preprocessing'

--- postprocessing/postprocessor.py
+++ postprocessing/postprocessor.py
@@ -7,7 +7,6 @@ from stroke2char import stroke2char, get_len
 from axis2img import axis2img
 from csv2txt import csv2txt
 from utils import argument_setting
-# from tqdm import tqdm
 
 def postprocessor():
 
@@ -46,24 +45,31 @@ def postprocessor():
                 target = pd.read_csv(os.path.join(dir_path, f'{file_feature}_target.csv'), header=None)
 
                 # get original length
-                if file_feature != 'test_all':
-                    stroke_len = get_len(target)
-                else:
-                    stroke_len = target.shape[0]
+                stroke_len = get_len(target)
                 
                 # drop the extra rows and read input and output data
                 target.drop(target.index[stroke_len:], inplace=True)
-                input = pd.read_csv(os.path.join(dir_path, f'{file_feature}_input.csv'), nrows=stroke_len, header=None)
-                output = pd.read_csv(os.path.join(dir_path, f'{file_feature}_output.csv'), nrows=stroke_len, header=None)
+                input = pd.read_csv(
+                            os.path.join(dir_path, f'{file_feature}_input.csv'),
+                            nrows=stroke_len,
+                            header=None
+                        )
+                output = pd.read_csv(
+                            os.path.join(dir_path, f'{file_feature}_output.csv'),
+                            nrows=stroke_len,
+                            header=None
+                        )
 
+                # test data but not test all data
                 if file_feature[:5] == 'test_' and file_feature != 'test_all':
-                    test_flag, test_target, test_input, test_output = stroke2char(
-                                                                            target.iloc[:, :-1],
-                                                                            input.iloc[:, :-1],
-                                                                            output.iloc[:, :-1],
-                                                                            test_target, test_input, test_output,
-                                                                            dir_path, stroke_len, file_feature[5:]
-                                                                        )
+                    (
+                        test_flag,
+                        test_target, test_input, test_output
+                    ) = stroke2char(
+                            target.iloc[:, :-1], input.iloc[:, :-1], output.iloc[:, :-1],
+                            test_target, test_input, test_output,
+                            dir_path, stroke_len, file_feature[5:]
+                        )
 
                 axis2img(target, input, output, file_feature, dir_path)
                 csv2txt(target, os.path.join(dir_path, f'{file_feature}_target.txt'))
