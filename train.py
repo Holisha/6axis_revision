fb16fbfa8d50c207d6a2d60928965aefc89573b0
Holisha <fan89511@gmail.com>
2020-08-11 01:31:06 +0800

    now different dataset have own progress bar but leave train set only. Update doc
    Update: In train.py progress modified
    Update: update doc parameters

--- train.py
+++ train.py
@@ -140,16 +140,17 @@ def train(model, train_loader, valid_loader, optimizer, criterion, args):
     if args.early_stop:
         early_stopping = EarlyStopping(patience=args.patience, verbose=args.verbose, threshold=args.threshold, path=model_path)
 
-    progress_bar = tqdm(total=len(train_loader)+len(valid_loader))
+    # progress_bar = tqdm(total=len(train_loader)+len(valid_loader))
 
     for epoch in range(checkpoint['epoch'], args.epochs+1):
         model.train()
         err = 0.0
         valid_err = 0.0
 
-        progress_bar.reset(total=len(train_loader)+len(valid_loader))        
-        progress_bar.set_description(f'Train epoch: {epoch}/{args.epochs}')
-        for data in train_loader:
+        # progress_bar.reset(total=len(train_loader)+len(valid_loader))        
+        # progress_bar.set_description(f'Train epoch: {epoch}/{args.epochs}')
+        train_bar = tqdm(train_loader, desc=f'Train epoch: {epoch}/{args.epochs}')
+        for data in train_bar:
             inputs, target, _ = data
             inputs, target = inputs.cuda(), target.cuda()
 
@@ -174,8 +175,9 @@ def train(model, train_loader, valid_loader, optimizer, criterion, args):
             loss = mse_loss + content_loss
 
             # update progress bar
-            progress_bar.set_postfix({'MSE loss': mse_loss.item(), 'Content loss': content_loss.item()})
-            progress_bar.update()
+            train_bar.set_postfix({'MSE loss': mse_loss.item(), 'Content loss': content_loss.item()})
+            # progress_bar.set_postfix({'MSE loss': mse_loss.item(), 'Content loss': content_loss.item()})
+            # progress_bar.update()
 
             err += loss.sum().item() * inputs.size(0)
 
@@ -184,13 +186,13 @@ def train(model, train_loader, valid_loader, optimizer, criterion, args):
             loss.backward()
             optimizer.step()
             
-
-
         # cross validation
-        progress_bar.set_description(f'Valid epoch:{epoch}/{args.epochs}')
+        # progress_bar.set_description(f'Valid epoch:{epoch}/{args.epochs}')
+        valid_bar = tqdm(valid_loader, desc=f'Valid epoch:{epoch}/{args.epochs}', leave=False)
         model.eval()
         with torch.no_grad():
-            for data in valid_loader:
+            for data in valid_bar:
+            # for data in valid_loader:
                 inputs, target, _ = data
                 inputs, target = inputs.cuda(), target.cuda()
 
@@ -215,8 +217,9 @@ def train(model, train_loader, valid_loader, optimizer, criterion, args):
                 loss = mse_loss + content_loss
 
                 # update tqdm info
-                progress_bar.set_postfix({'MSE loss': mse_loss.sum().item(), 'Content loss': content_loss.sum().item()})
-                progress_bar.update()
+                valid_bar.set_postfix({'MSE loss': mse_loss.sum().item(), 'Content loss': content_loss.sum().item()})
+                # progress_bar.set_postfix({'MSE loss': mse_loss.sum().item(), 'Content loss': content_loss.sum().item()})
+                # progress_bar.update()
 
                 valid_err += loss.sum().item() * inputs.size(0)
 
@@ -267,7 +270,7 @@ def train(model, train_loader, valid_loader, optimizer, criterion, args):
                 break
 
     writer.close()
-    progress_bar.close()
+    # progress_bar.close()
 
 
 if __name__ == '__main__':
