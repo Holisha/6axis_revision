5cd130eafdddd9b88c2dbae45f3f90195e25f865
lyjeff <38328932+lyjeff@users.noreply.github.com>
2020-11-26 23:23:37 +0800

    Update README.md and rename demo.py and project.py

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # 6 Axis Revision 
 
+- [6 Axis Revision](#6-axis-revision)
+  - [Introduction](#introduction)
+  - [Folder structure](#folder-structure)
+  - [Requirements and Dependencies](#requirements-and-dependencies)
+  - [Demo](#demo)
+  - [Training](#training)
+    - [model parameter](#model-parameter)
+  - [Pretained model](#pretained-model)
+  - [Eval](#eval)
 ## Introduction
 
 - This project is aiming at revise calligraphy words based on CNN method.
@@ -9,85 +18,140 @@
 ```
    \---6axis_revision
     |   .gitignore
-    |   __init__.py
     |   LOG.md
     |   README.md
-    |   axis2img.py
-    |   dataset.py
-    |   get_6d_3d.py
-    |   s.txt
-    |   test.py
-    |   test_error.py
-    |   tmp.py
-    |   train.py
-    |   train_error.py
-    |   train_recurrent.py
-    |   utils.py
-    |   
+    |
+    +---demo
+    |   |   .gitignore
+    |   |   README.md
+    |   |   __init__.py
+    |   |   bg.qrc
+    |   |   demo.py
+    |   |   demo.ui
+    |   |   demo_utils.py
+    |   |   project.py
+    |   |   
+    |   +---calligraphy
+    |   |   |   __init__.py
+    |   |   |   calligraphy_transform.py
+    |   |   |   char_list.csv
+    |   |   |   code.py
+    |   |   |   
+    |   |   +---utils
+    |   |           __init__.py
+    |   |           tools.py
+    |   |           
+    |   +---imgs
+    |           BING.jpg
+    |           SHA.jpg
+    |           SHI.jpg
+    |           YIN.jpg
+    |           YONG.jpg
+    |           YONG.png
+    |           YONG_input.png
+    |           dot-removebg-preview.png
+    |           images-removebg-preview.png
+    |           loading.gif
+    |           mm.png
+    |           monkey.png
+    |           mountain.png
+    |           red_blue.png
+    |           
     +---doc
     |       sampleV4.json
     |       sampleV4.yaml
     |       
-    +---light
-    |       fsrcnn.py
-    |       
-    +---model
-    |   |   __init__.py
-    |   |   loss.py
-    |   |   optimizer.py
-    |   |   
-    |   +---DBPN
-    |   |       __init__.py
-    |   |       models.py
-    |   |           
-    |   +---FSRCNN
-    |           __init__.py
-    |           models.py
-    |         
-    |              
-    |           
-    +---postprocessing
-    |       .gitignore
-    |       __init__.py
-    |       README.md
-    |       axis2img.py
-    |       csv2txt.py
-    |       postprocessor.py
-    |       post_utils.py
-    |       stroke2char.py
-    |       
-    +---preprocessing
-            .gitignore
-            preprocess.py
-            readme.md
-            requirement.txt
-            utils.py
+    +---src
+        |   __init__.py
+        |   dataset.py
+        |   eval.py
+        |   test_error.py
+        |   train.py
+        |   train_error.py
+        |   train_recurrent.py
+        |   utils.py
+        |   
+        +---light
+        |       fsrcnn.py
+        |       
+        +---model
+        |   |   __init__.py
+        |   |   loss.py
+        |   |   optimizer.py
+        |   |   
+        |   +---DBPN
+        |   |       __init__.py
+        |   |       models.py
+        |   |           
+        |   +---FSRCNN
+        |           __init__.py
+        |           models.py
+        |           
+        +---postprocessing
+        |       .gitignore
+        |       README.md
+        |       __init__.py
+        |       axis2img.py
+        |       csv2txt.py
+        |       post_utils.py
+        |       postprocessor.py
+        |       stroke2char.py
+        |       verification.py
+        |       
+        +---preprocessing
+                .gitignore
+                __init__.py
+                pre_utils.py
+                preprocessor.py
+                readme.md
 ```
 
-## TODO
-
-- update readme file
-- add pretain model
-- add visualization image
-- add demo
-- add guide file
 
 ## Requirements and Dependencies
 
 - This project runs on gpu only
-- `pip install requirements.txt`
 - CUDA 10.1
 - Python 3.6 or upper version
+    ```shell
+    pip install requirements.txt
+    ```
 
+## Demo
+- To run the demo of evaluating process
+    1. Copy `dataset/` in `demo.tar.gz` to `6axis_revision/` .
+   
+    2. Copy `logs/` in `demo.tar.gz` to `demo/` .
+
+    3. Move to `demo/` folder.
+
+    4. Run the following command in shell.
+        ```shell
+        python demo.py
+        ```
+
+    5. Input the character number and the range of noise.
+
+    6. Program will display the test loss on the screen.
+
+- You could add `--gui` argument to run for GUI, also add `--usb-path` to store the demo files to USB or others path . Example:
+    ```shell
+    python demo.py --gui --usb-path USB_PATH
+    ```
+
+- The evaluating result, including input, output and target of Robot command file, and 2D visualization compare picture, will store in `demo/output/test_char/`.
 ## Training
 
 - To train with document file
-`python train.py --doc`
+    ```
+    python train.py --doc
+    ```
 
 - To train with argument vector
-`python train.py --gpu-id 0 ...`
+    ```
+    python train.py --gpu-id 0 ...
+    ```
 
-- please check `doc/sample` in detail
+- please check `doc/sampleV4.yaml` in detail
 
 ### model parameter
 
@@ -96,7 +160,6 @@
     - DBPN: 1e-4
 
 ```python
-
 DBPN-S:
 	stages=2,
 	n0=128,
@@ -120,7 +183,6 @@ ADBPN-SS:
 	stages=2,
 	n0=64,
 	nr=18
-}
 ```
 
 ## Pretained model
@@ -137,6 +199,3 @@ ADBPN-SS:
 
 - To evaluate the model by
 `python test.py --gpu-id 0 ...`
-
-- To visualize model's prediction
-    - To be implement
--- demo/project.py
+++ demo/project.py
@@ -1,91 +1,287 @@
-import sys,os
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.uic import loadUi
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5 import QtCore,QtGui,QtWidgets,QtWebEngineWidgets
-from demo import demo_main, model_env
-from calligraphy.code import draw_pic
-from demo_utils import argument_setting
+import os, sys, shutil
+import torch
+from torch.utils.data import DataLoader
+from argparse import ArgumentParser
 
-windows = []
-meetingUrl="https://meet.google.com/ose-krmk-zzg"
-noise=0
-word_idx=42
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        QtWidgets.QMainWindow.setFixedSize(self,1600,400)
-        self.webview = WebEngineView()
-        self.webview.load(QtCore.QUrl(meetingUrl))
-        self.setCentralWidget(self.webview)
-class WebEngineView(QtWebEngineWidgets.QWebEngineView):
-    windows = [] #创建一个容器存储每个窗口，不然会崩溃，因为是createwindow函数里面的临时变量
-    def createWindow(self, QWebEnginePage_WebWindowType):
-        newtab =   WebEngineView()
-        newwindow= MainWindow()
-        newwindow.setCentralWidget(newtab)
-        newwindow.show()
-        self.windows.append(newwindow)
-        return newtab
-        
-class ExComboBox(object):
-    def __init__(self,w):
-        print(w.comboBox.currentText())
-        w.comboBox.activated[str].connect(self.words)
-    def words(self):
-        global word_idx
-        w.input.setPixmap(QPixmap("./imgs/loading.gif"))
-        w.slim.setPixmap(QPixmap("./imgs/loading.gif"))
-        print(w.comboBox.currentText())
-        if w.comboBox.currentText() == "永":
-            w.target.setPixmap(QPixmap("imgs/YONG.jpg"))
-            word_idx = 42
-        elif w.comboBox.currentText() == "史":
-            w.target.setPixmap(QPixmap("imgs/SHI.jpg"))
-            # w.slim.setPixmap(QPixmap("output/test_char/test_all_compare.png"))
-            word_idx = 436
-        elif w.comboBox.currentText() == "殺":
-            w.target.setPixmap(QPixmap("imgs/SHA.jpg"))
-            word_idx = 312
-        elif w.comboBox.currentText() == "并":
-            w.target.setPixmap(QPixmap("imgs/BING.jpg"))
-            word_idx = 773
-        elif w.comboBox.currentText() == "引":
-            w.target.setPixmap(QPixmap("imgs/YIN.jpg"))
-            word_idx = 277
- 
-def go_web(args):
+# system path
+sys.path.append(r'../src/')
+sys.path.append(r'../src/postprocessing')
+sys.path.append(r'../src/preprocessing')
+sys.path.append(r'../src/model')
+
+# self defined
+from demo_utils import (argument_setting, timer, demo_post)
+from preprocessing import preprocessor
+from postprocessing import (postprocessor, verification)
+from model import FeatureExtractor
+from eval import test, demo_eval
+
+# test defined
+from utils import  (model_builder, model_config, config_loader, criterion_builder)
+from dataset import AxisDataSet
+
+# execution statistics
+exe_stat = []
+
+def model_env(args):
+    """building model environment avoiding to instantiate model.
+
+    Args:
+        args : model arguments which is control by demo_utils.argument_setting
+
+    Returns:
+        model (torch.nn): build model in cuda device
+        criterion(torch.nn): build criterion. Default to mse loss
+        extractor(torch.nn): build vgg content loss in cuda device
+    """
+
+    if args.doc:
+        args = config_loader(args.doc, args)
+
+    # set cuda device
+    torch.cuda.set_device(args.gpu_id)
+
+    # model version control
+    version = args.load if type(args.load) is int else 0
+
+    # model path and parameter
+    model_path = os.path.join(
+        args.log_path, args.model_name, f'version_{version}',f'{args.model_name}_{args.scale}x.pt')
+
+    checkpoint = torch.load(model_path, map_location=f'cuda:{args.gpu_id}')
+
+    # loading model
+    model = model_builder(args.model_name, args.scale, **args.model_args).cuda()
+    model.load_state_dict(checkpoint['state_dict'])
+
+    # build criterion
+    criterion = criterion_builder(args.criterion)
+
+    # loading feature extractor
+    extractor = FeatureExtractor().cuda() if args.content_loss else None
+
+    return model, criterion, extractor
+
+
+def data_env(args):
+    """build data loader
+
+    Args:
+        args : model arguments which is control by demo_utils.argument_setting
+
+    Returns:
+        data_loader [torch.utils.data.DataLoader]: dataloader for only one character
+    """
+    dataset = AxisDataSet(args.test_path, args.target_path)
+
+    data_loader = DataLoader(dataset,
+                             batch_size=args.batch_size,
+                             shuffle=False,
+                             num_workers=args.num_workers,
+                            #  pin_memory=True,
+                             pin_memory=False,
+                            )
     
-    noise = w.doubleSpinBox.value()
-    demo_main(args, noise, word_idx)
-    draw_pic()
-    w.slim.setPixmap(QPixmap("output/test_char/test_all_compare.png"))
-    w.input.setPixmap(QPixmap("./output/visual/test_all_input.png"))
-    # newtab =   WebEngineView()
-    # newtab.load(QtCore.QUrl(meetingUrl))
-    # newwindow= MainWindow()
-    # newwindow.setCentralWidget(newtab)
-    # newwindow.show()
-    # windows.append(newwindow)
-    
+    return data_loader
 
-if __name__ == "__main__":
-    args = argument_setting()
-
-    # construction env first
-    if not args.nonefficient:
-        args.model, args.critetion, args.extractor = model_env(args)
-
+@timer
+def efficient_demo(args,noise=0,test_char=42):
+    # Input the number of the character
     if args.gui:
-        app = QApplication(sys.argv)
-        w = loadUi('demo.ui')
-        windows.append(w)
-        ui=ExComboBox(w)
-        w.label.setScaledContents(True)
-        w.pushButton.clicked.connect(lambda: go_web(args))
-        # w.showFullScreen()
-        # w.setFixedSize(800,800)
-        w.show()
-        app.exec_()
+        args.test_char = test_char
     else:
-        demo_main(args)
+        test_char = 0
+        while test_char < 1 or test_char > args.char_max:
+            test_char = int(input(f"Input the number of character (1-{args.char_max}):"))
+            if test_char < 1 or test_char > args.char_max:
+                print("Please input the number in the correct range!")
+            else:
+                args.test_char = test_char
+                break
+    
+    # Input the upper limit of the noise range
+    if args.gui:
+        args.noise = [-1 * noise, noise]
+    else:
+        noise = -1
+        while noise < 0 or noise > args.noise_max:
+            noise = float(input(f"Input the upper limit of the noise range (0-{args.noise_max}):"))
+            if noise < 0 or noise > args.noise_max:
+                print("Please input the number in the correct range!")
+            else:
+                args.noise = [-1 * noise, noise]
+                break
+    
+    if os.path.exists(args.test_path):
+        shutil.rmtree(args.test_path)
+    if os.path.exists(args.save_path):
+        shutil.rmtree(args.save_path)
+
+    exe_stat.append(
+        preprocessor(args)
+    )
+
+    print('\n===================================================')
+
+    # construction dataset
+    data_loader = data_env(args)
+
+    exe_stat.append(
+        demo_eval(args.model, data_loader, args.critetion, args, args.extractor)
+    )
+
+    print('\n===================================================')
+    exe_stat.append(
+        postprocessor(args)
+    )
+
+    print('\n===================================================')
+    exe_stat.append(
+        verification(args)
+    )
+
+    if args.usb_path != None:
+        print('\n===================================================')
+        exe_stat.append(
+            demo_post(args)
+        )
+
+    print('\n===================================================')
+    print(f'Testing number {args.test_char} with noise {args.noise}, Done!!!')
+
+
+def demo_test(args):
+    if args.doc:
+        args = config_loader(args.doc, args)
+    # config
+    # model_config(args, save=False)     # print model configuration of evaluation
+
+    # set cuda
+    torch.cuda.set_device(args.gpu_id)
+
+    # model
+    model = model_builder(args.model_name, args.scale, **args.model_args).cuda()
+
+    # criteriohn
+    criterion = criterion_builder(args.criterion)
+
+    # dataset
+    test_set = AxisDataSet(args.test_path, args.target_path)
+
+    test_loader = DataLoader(test_set,
+                             batch_size=args.batch_size,
+                             shuffle=False,
+                             num_workers=args.num_workers,
+                            #  pin_memory=True,
+                             pin_memory=False,
+                             )
+
+    # test
+    test(model, test_loader, criterion, args)
+
+@timer
+def demo(args,noise=0,test_char=42):
+
+    # Input the number of the character
+    if args.gui:
+        args.test_char = test_char
+    else:
+        test_char = 0
+        while test_char < 1 or test_char > args.char_max:
+            test_char = int(input(f"Input the number of character (1-{args.char_max}):"))
+            if test_char < 1 or test_char > args.char_max:
+                print("Please input the number in the correct range!")
+            else:
+                args.test_char = test_char
+                break
+    
+    # Input the upper limit of the noise range
+    if args.gui:
+        args.noise = [-1 * noise, noise]
+    else:
+        noise = -1
+        while noise < 0 or noise > args.noise_max:
+            noise = float(input(f"Input the upper limit of the noise range (0-{args.noise_max}):"))
+            if noise < 0 or noise > args.noise_max:
+                print("Please input the number in the correct range!")
+            else:
+                args.noise = [-1 * noise, noise]
+                break
+
+    if os.path.exists(args.test_path):
+        shutil.rmtree(args.test_path)
+    if os.path.exists(args.save_path):
+        shutil.rmtree(args.save_path)
+
+    exe_stat.append(
+        preprocessor(args)
+    )
+
+    print('\n===================================================')
+    exe_stat.append(
+        demo_test(args)
+    )
+
+    print('\n===================================================')
+    exe_stat.append(
+        postprocessor(args)
+    )
+
+    print('\n===================================================')
+    exe_stat.append(
+        verification(args)
+    )
+
+    if args.usb_path != None:
+        print('\n===================================================')
+        exe_stat.append(
+            demo_post(args)
+        )
+
+    print('\n===================================================')
+    print(f'Testing number {args.test_char} with noise {args.noise}, Done!!!')
+
+def demo_main(args, noise=0, word_idx=42):
+    # argument setting
+    # args = argument_setting()
+
+    # config
+    # model_config(args, save=False)   # print model configuration of evaluation
+    
+    # attach timer function
+    if args.timer:
+        preprocessor = timer(preprocessor)
+
+        # differnet demo env
+        demo_test = timer(demo_test)
+        demo_eval = timer(demo_eval)
+
+        postprocessor = timer(postprocessor)
+        verification = timer(verification)
+        copy2usb = timer(copy2usb)
+        translation = timer(translation)
+
+    # execution main function
+    demo_func = efficient_demo if not args.nonefficient else demo
+    if args.gui:
+        demo_func(args,noise,word_idx)
+    else:
+        demo_func(args)
+
+    # timer statistics
+    if args.timer:
+        import pandas as pd
+        stat = pd.DataFrame(exe_stat, index=['preprocessor', 'demo_efficient', 'postprocessor', 'verification', 'translation', 'copy2usb'], columns=['time'])
+
+        stat['percent'] = stat / stat.sum() * 100
+
+        stat = stat.round(
+            {'time': 2, 'percent': 2,}
+        )
+        print(f'\nperformance statistics:\n{stat}')
+        print(f'total execution time: {stat["time"].sum()}')
+
+if __name__ == '__main__':
+    # argument setting
+    args = argument_setting()
+    demo_main(args)
